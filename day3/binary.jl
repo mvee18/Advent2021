ee15d7144ae5ee78b3b86d1ad9bfd3f185c29c53
mvee18 <emvalenc@go.olemiss.edu>
2021-12-10 19:12:29 -0600

    work in progress

--- day3/binary.jl
+++ day3/binary.jl
@@ -1,3 +1,5 @@
+using DelimitedFiles
+
 function createArray(filepath::String)
     gamma = ""
     epsilon = ""
@@ -26,4 +28,58 @@ function createArray(filepath::String)
 
 end
 
-createArray("test.txt")
+function generateArrayFromText(filepath::String)
+	newMat = []
+	m = readdlm(filepath, ' ', Int, '\n')
+
+	for x in m
+		push!(newMat, digits(x, pad=5))
+	end
+
+	return newMat
+end
+
+function getCols(m)
+	tot = sum(m, dims = 1)
+	gamma = ""    
+	epsilon = ""
+
+	rev = reverse(tot[1])
+	println(rev, size(m, 1))
+	for i in rev
+		println(i, " ", size(m, 1)-i)
+		if (i > size(m, 1)-i)
+			gamma = string(gamma, "1")
+			epsilon = string(epsilon, "0")
+		else
+			gamma = string(gamma, "0")
+			epsilon = string(epsilon, "1")
+		end
+	end
+
+	return gamma, epsilon
+end
+
+g, e = getCols(generateArrayFromText("test.txt"))
+
+
+# println(g, " ", e)
+# println((parse(Int64, g, base=2) * parse(Int64, e, base=2)))
+
+test = generateArrayFromText("test.txt")
+
+function filterArrayOxygen(m)
+	tot = sum(m, dims = 1)
+	rev = reverse(tot[1])
+	for i in rev
+		# If one is the prevalent value...
+		if (i > size(m, 1)-i)
+		else
+
+		end
+	end
+end
+
+function filterArray(m, index, val)
+end
+
